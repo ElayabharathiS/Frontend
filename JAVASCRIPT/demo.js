db99579dad2e06a070a19bfe28beed1df5dcd6ae
ElayabharathiS <psebharathi@gmail.com>
2025-07-19 20:43:35 +0530

    adding all files to git repo

---
+++ JAVASCRIPT/demo.js
@@ -0,0 +1,12 @@
+var favActor="Ajith";
+var favPlayer="Hardik";
+var favMovie="Batman";
+
+function favourite()
+{
+    console.log("Favourite Actor: "+ favActor)
+    console.log("Favourite Player: "+ favPlayer)
+    console.log("Favourite Movie: "+ favMovie)
+}
+
+favourite()
