77f3c1e16baeceeb874cc74c14deaf186359ed91
ElayabharathiS <psebharathi@gmail.com>
2025-08-07 20:53:36 +0530

    updated

---
+++ JAVASCRIPT/test.js
@@ -0,0 +1,7 @@
+try{
+  console.log(test);
+}catch(error){
+   console.log(error.message)
+   console.log(error.name)
+   console.log(error.stack)
+}
---
+++ ReactJS/React-App/src/navbar.jsx
@@ -0,0 +1,10 @@
+function NavBar(){
+    return(
+        <nav>
+            <h2>Code.io</h2>
+            <button>Login</button>
+        </nav>
+    );
+}
+
+export default NavBar
