fc717c88d85aa209e141da40c09d6ced2cc0c018
ElayabharathiS <psebharathi@gmail.com>
2025-08-12 19:13:34 +0530

    updated

--- ReactJS/React-App/src/course.jsx
+++ ReactJS/React-App/src/course.jsx
@@ -1,7 +1,12 @@
+const course1 = "HTML";
+
 function Course(){
     return(
-         <div></div>
-
-
+         <div>
+            <h3>{course1}</h3>
+            <p>This is code io's HTML course</p>
+         </div>
     );
 }
+
+export default Course
